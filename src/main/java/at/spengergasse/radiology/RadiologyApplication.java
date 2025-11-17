e59494f07e951edb41e2d66f31df9dde3a700bbd
Rudolf Radlbauer <radlbauer@spengergasse.at>
2025-11-17 15:29:17 +0100

    Initial commit

---
+++ src/main/java/at/spengergasse/radiology/RadiologyApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.radiology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadiologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/radiology/RadiologyApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.radiology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
